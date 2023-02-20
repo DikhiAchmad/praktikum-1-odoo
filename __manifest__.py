a082b322162d01a36ffcf05c8116eeae5d2a4b23
DikhiAchmad <dikia656@gmail.com>
2023-02-20 14:05:21 +0700

    init: addons module

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "New Addons",
+    "version": "1.0",
+    "author": "Dikhi Achmad Dani",
+    "website": "https://www.detik.com/",
+    "category": "New Module",
+    "summary": "ini adalah addons baru",
+    "description":""" 
+        Latihan membuat addons baru di odoo 
+    """,
+    "depends": [
+        "base",
+    ],
+    "data": [
+
+    ],
+    "demo": [],
+    "test": [],
+    "image": [],
+    "qweb": [],
+    "css": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+
+}
